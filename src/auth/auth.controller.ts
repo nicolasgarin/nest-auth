88513a835c07a98b65c897f98e0b906e2a68084c
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-09-03 16:16:43 -0300

    .

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signup() {
+    return 'signup route';
+  }
+
+  @Post('signin')
+  signin() {
+    return 'signin route';
+  }
+
+  @Get('signout')
+  signout() {
+    return 'signout route';
+  }
+}
